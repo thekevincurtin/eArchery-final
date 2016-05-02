ed6a6017ddf269c2b7cc554d20886dcba8d6152f
Kevin Curtin <thekevincurtin@gmail.com>
2016-05-02 08:53:57 -0400

    t

--- src/com/redbear/chat/Practice.java
+++ src/com/redbear/chat/Practice.java
@@ -1,16 +1,37 @@
 package com.redbear.chat;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class Practice extends Activity {
     private ListView listView;
+    private boolean isRecording = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
+
+        final Button shotButton = (Button)findViewById(R.id.shot_button);
+        shotButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isRecording){
+                    //begin recording
+                    isRecording = true;
+                    shotButton.setText("Stop Shot");
+                    //append data to array
+                }
+                else if(isRecording){
+                    //stop recording & analyze data
+                    isRecording = false;
+                    shotButton.setText("Start Shot");
+                }
+            }
+        });
 
         listView = (ListView) findViewById(R.id.list_view);
         String[] values = new String[]{"Good","Test2","test420"};
