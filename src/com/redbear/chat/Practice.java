84ed5994044ebcc9307e97eeccb01fe71907b42c
Kevin Curtin <thekevincurtin@gmail.com>
2016-05-02 08:39:58 -0400

    t

--- src/com/redbear/chat/Practice.java
+++ src/com/redbear/chat/Practice.java
@@ -2,13 +2,26 @@ package com.redbear.chat;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Practice extends Activity {
-
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
+
+        listView = (ListView) findViewById(R.id.list_view);
+        String[] values = new String[]{"Good","Test2","test420"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        listView.setAdapter(adapter);
+        //update
+        values = new String[]{"updated","updated","updated","updated","updated","updated","updated","updated","updated","updated",
+                "updated","updated","updated","updated"};
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        listView.setAdapter(adapter);
     }
 
 }
